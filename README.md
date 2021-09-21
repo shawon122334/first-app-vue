a9d01f96d91a00e094827da36b949136d3bf6d27
shawon122334 <shawon.mu11@gmail.com>
2021-09-21 13:20:22 +0600

    directive done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # first-app-vue
+things I learned from my first vue js applications are : 
+    1.instance 
+    2.methods (we can access the datas using this keyword)
+    3.v-bind (directives : to use data in method )
+    4.v-text
+    5.v-html
+    6.conditional rendering 
+    7.
--- app.js
+++ app.js
@@ -2,12 +2,19 @@ new Vue({
     el:"#app",
     data: {
         title : "Sabbir",
-        cars: ['Bently','Toyota'],
-        robot: {
-            name : "Frank"
+        bikes: ['Yamaha','Honda'],
+        books: {
+            name : "C programming by subeen "
         },
-        greeting:()=>{
-            return "Hello World";
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+        alt: 'this is an image',
+        link: 'https://google.com',
+        hi: 'hi this is vue js',
+        yes: "<strong>Hello there</strong>" 
+    },
+    methods:{
+        hello(){
+            return this.books.name;
         }
     }
 });
